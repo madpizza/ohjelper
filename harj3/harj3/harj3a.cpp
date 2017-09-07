5bdfa11cc371b1cad26e056768d8702fae3f2a04
Miikka Veijola <H9392@LABRANET.JAMK.FI>
2017-09-07 15:37:55 +0300

    harjoitus 3:n soossit

---
+++ harj3/harj3/harj3a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout
+		<< "Miikka Veijola"
+		<< endl
+		<< "Veturitallinkatu 5\n";
+}
---
+++ harj3/harj3/harj3b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main () {
+	cout 
+		<< "***********************"
+		<< endl
+		<< "* *\n"
+		<< endl
+		<< "* Miikka *"
+		<< endl
+		<< "* Veijola *"
+		<< endl
+		<< "*_____________________*"
+		<< endl
+		<< "* *"
+		<< endl
+		<< "* Veturitallinkatu 5 *"
+		<< endl
+		<< "* 40100 JKL *"
+		<< endl
+		<< "* *"
+		<< endl
+		<< "***********************\n";
+}
